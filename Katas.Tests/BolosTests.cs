94fdb2fd6aa146f51522b85c55203b938dd7ca7f
Alejandro Prieto <jose.prieto@sinco.com.co>
2025-10-15 11:52:10 -0500

    Second commit

--- Katas.Tests/BolosTests.cs
+++ Katas.Tests/BolosTests.cs
@@ -9,24 +9,43 @@ public class BolosTests
     public void Si_IniciaPartida_Debe_PuntajeSerCero()
     {
         //Arrange
-        var partida = new Marcador();
+        var marcador = new Marcador();
         
         //Assert
-        partida.Puntaje.Should().Be(0);
+        marcador.Puntaje.Should().Be(0);
     }
 
     [Fact]
-    public void Si_TumboCeroPinesPrimerRollYCeroPinesSegundoRoll_Debe_PuntajeSerCero()
+    public void Si_TumboCeroPinesEnElTurno_Debe_PuntajeSerCero()
     {
         //Arrange
+        var marcador = new Marcador();
+        
+        //Act
+        marcador.RegistrarPuntaje(0,0);
+        
+        //Assert
+        marcador.Puntaje.Should().Be(0);
+    }
+
+    [Fact]
+    public void Si_TumboUnPinEnElPrimerRollYDosPinesEnElSegundoRoll_Debe_PuntajeSerTres()
+    {
+        // Arrange
         
         //Act
         
-        //Assert
+        // Assert
     }
 }
+
 
 public class Marcador
 {
     public int Puntaje { get; private set; } = 0;
+    
+    public void RegistrarPuntaje(int rollUno, int rollDos)
+    {
+        Puntaje = rollUno + rollDos;
+    }
 }
--- Katas.Tests/TestList.md
+++ Katas.Tests/TestList.md
@@ -1,9 +1,9 @@
 # Puntuación de Bolos
 
 - [x] Al iniciar la partida, el puntaje inicial es 0.
-- [ ] Si tumbo 0 pines en el primer roll y 0 pines en el segundo, mi puntuación de ese turno debe ser 0.
-- [ ] Cada turno debe derribar como máximo 10 pines. 
-- [ ] Si en un turno tumbo 1 pin en el primer roll y 2 en el segundo roll, mi puntuación de ese turno debe ser 3.
+- [x] Si tumbo 0 pines en el primer roll y 0 pines en el segundo, mi puntuación de ese turno debe ser 0.
+- [ ] Si tumbo 1 pin en el primer roll y 2 en el segundo roll, mi puntuación de ese turno debe ser 3.
+- [ ] Cada turno debe derribar como máximo 10 pines.
 - [ ] Si no derrumbo ningún pin, mi puntuación es 0.
 - [ ] Si derribo un pin en cada turno, mi puntuación es 10.
 - [ ] Si en el primer turno mi puntaje es de 6 y en el segundo turno mi puntaje es 8, mi puntaje total es 14.
