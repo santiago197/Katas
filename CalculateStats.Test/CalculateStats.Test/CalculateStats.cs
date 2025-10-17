7bfde3e55386f1a7e6e8d08d24a951b60280376f
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-10-17 13:45:56 -0500

    🚨Si_Recibe_Secuencia_De_Numeros_Debe_Retornar_El_Valor_Minimo_y_Maximo

---
+++ CalculateStats.Test/CalculateStats.Test/CalculateStats.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace CalculateStats.Test;
+
+public class CalculateStats
+{
+    [Fact]
+    public void Si_Recibe_Secuencia_De_Numeros_Debe_Retornar_El_Valor_Minimo_y_Maximo()
+    {
+        //Arrange
+        var calcular = new Calcular();
+        var numeros = [1, 3, 9];
+        
+        //Act 
+        var resultado = calcular.ValorMaximoyMinimo();
+        //Assert
+        resultado.Should().BeEqual(1, 9);
+    }
+}
--- CalculateStats.Test/CalculateStats.Test/obj/Debug/net9.0/CalculateStats.Test.AssemblyInfo.cs
+++ CalculateStats.Test/CalculateStats.Test/obj/Debug/net9.0/CalculateStats.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculateStats.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8bf992a3ada39732f332aac788cea4b2cc4655")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculateStats.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculateStats.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
