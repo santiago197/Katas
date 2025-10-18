3981733a864b981c970e8210734ec096dc055813
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-10-17 20:24:06 -0500

    🩹Si_Recibe_Un_Anio_Bisiesto_Debe_Retornar_True

--- CalculateStats.Test/LeapYears/LeapYears.cs
+++ CalculateStats.Test/LeapYears/LeapYears.cs
@@ -5,17 +5,17 @@ namespace LeapYears;
 public class LeapYears
 {
     [Theory]
-    [InlineData(1900)]
-    [InlineData(2001)]
-    [InlineData(1996)]
-    [InlineData(2000)]
-    public void Si_Recibe_Un_Anio_Bisiesto_Debe_Retornar_True(int anio)
+    [InlineData(1900, true)]
+    [InlineData(2001, false)]
+    [InlineData(1996, true)]
+    [InlineData(2000, true)]
+    public void Si_Recibe_Un_Anio_Bisiesto_Debe_Retornar_True(int anio, bool esperado)
     {
         var calcular = new Calcular();
 
         var resultado = calcular.CalculaAnioBisiesto(anio);
 
-        resultado.Should().Be(true);
+        resultado.Should().Be(esperado);
     }
 }
 
@@ -23,9 +23,11 @@ public class Calcular
 {
     public bool CalculaAnioBisiesto(int anio)
     {
-        var esAnioBisiesto = ((anio == 1900) || (anio == 1900) || (anio == 2001) || (anio == 1996) || (anio == 2000))
-            ? true
-            : false;
-        return esAnioBisiesto;
+        return CalcularAnioBisiesto(anio);
+    }
+
+    private static bool CalcularAnioBisiesto(int anio)
+    {
+        return anio % 4 == 0 || (anio % 100 != 0) && (anio % 400 == 0);
     }
 }
--- CalculateStats.Test/LeapYears/obj/Debug/net9.0/LeapYears.AssemblyInfo.cs
+++ CalculateStats.Test/LeapYears/obj/Debug/net9.0/LeapYears.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeapYears")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5261f5c14ff6e7d298133d10764db7179d76ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2967dbf0a040df2e367c5649f82ed44294c38a33")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeapYears")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeapYears")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
