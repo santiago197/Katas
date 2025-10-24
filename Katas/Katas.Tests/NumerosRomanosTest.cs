8045a5aa5f3ef4aaa6d9c11cf2763b7f97186ebc
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-10-24 11:37:55 -0500

    🔴Si_RecibeNumeroUno_Debe_Retornar_I

---
+++ Katas/Katas.Tests/NumerosRomanosTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+
+namespace Katas.Tests;
+
+public class NumerosRomanosTest
+{
+    [Fact]
+    public void Si_RecibeNumeroUno_Debe_Retornar_I()
+    {
+        var romanos = new NumerosRomanos();
+
+        var resultado = romanos.Convertir(1);
+
+        resultado.Should
+            ().Be("I");
+    }
+}
+
+public class NumerosRomanos
+{
+    public object Convertir(int i)
+    {
+        throw new NotImplementedException();
+    }
+}
