03db40ae4a6ebd93b6750c42baf8fc6950f95b2d
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-11-11 18:58:31 -0500

    🔴🟢🔵Si_ImprimoSinAgregarProductos_Debe_ImprimirReciboEnBlanco

---
+++ Supermercado.Tests/Supermercado.Tests.cs
@@ -0,0 +1,30 @@
+﻿using AwesomeAssertions;
+
+namespace Supermercado.Tests;
+
+public class SupermercadoTests
+{
+    [Fact]
+    public void Si_ImprimoSinAgregarProductos_Debe_ImprimirReciboEnBlanco()
+    {
+        var supermercado = new Supermercado();
+        var reciboEsperado = "--------------------------------------------\r\n" +
+                             "| Producto     | Total  | Descuento |\r\n" +
+                             "| Precio total: 0                     |";
+
+        supermercado.AgregarProducto();
+
+        supermercado.Recibo.Should().Be(reciboEsperado);
+    }
+}
+
+public class Supermercado
+{
+    public void AgregarProducto()
+    {
+    }
+
+    public string Recibo { get; set; } = "--------------------------------------------\r\n" +
+                                         "| Producto     | Total  | Descuento |\r\n" +
+                                         "| Precio total: 0                     |";
+}
