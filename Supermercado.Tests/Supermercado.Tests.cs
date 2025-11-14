7f4d0c678835eff8f49eac76546546491415c566
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-11-14 09:27:46 -0500

    🔴🟢Si_TextoEsVacioyColEs1_Debe_RetornarVacio

--- Supermercado.Tests/Supermercado.Tests.cs
+++ Supermercado.Tests/Supermercado.Tests.cs
@@ -10,23 +10,56 @@ public class SupermercadoTests
         var supermercado = new Supermercado();
         var reciboEsperado = "--------------------------------------------\r\n" +
                              "| Producto     | Total  | Descuento |\r\n" +
-                             "| Precio total: 0                     |";
-
-        supermercado.AgregarProducto();
+                             "| Cepillo de dientes     | 0.99  | 0 |\r\n" +
+                             "| Precio total: 0                    |";
 
         supermercado.Recibo.Should().Be(reciboEsperado);
     }
 
-   
+    [Fact]
+    public void Si_Agrego1CepilloDeDientes_Debe_SerReciboConDetalle()
+    {
+        var supermercado = new Supermercado();
+        var reciboEsperado = "--------------------------------------------\r\n" +
+                             "| Producto     | Total  | Descuento |\r\n" +
+                             "| Cepillo de dientes     | 0.99  | 0 |\r\n" +
+                             "| Precio total: 0                    |";
+
+        supermercado.AgregarProducto(new CepilloDientes());
+
+        supermercado.Recibo.Should().Be(reciboEsperado);
+    }
+
+    [Fact]
+    public void X()
+    {
+        var supermercado = new Supermercado();
+
+        supermercado.AgregarProducto(new TomatesCherry());
+    }
+}
+
+public record TomatesCherry : Producto
+{
+}
+
+public record CepilloDientes : Producto
+{
 }
 
 public class Supermercado
 {
-    public void AgregarProducto()
+    public void AgregarProducto(Producto producto)
     {
     }
 
     public string Recibo { get; set; } = "--------------------------------------------\r\n" +
                                          "| Producto     | Total  | Descuento |\r\n" +
-                                         "| Precio total: 0                     |";
+                                         "| Cepillo de dientes     | 0.99  | 0 |\r\n" +
+                                         "| Precio total: 0                    |";
 }
+
+public record Producto
+{
+    public int Valor { get; set; }
+};
---
+++ WordWrap.Tests/WordWrapTests.cs
@@ -0,0 +1,83 @@
+using AwesomeAssertions;
+
+namespace WordWrap.Tests;
+
+public class WordWrapTests
+{
+    [Fact]
+    public void Si_TextoEsVacioyColEs1_Debe_RetornarVacio()
+    {
+        var result = Wrap("", 1);
+
+        result.Should().Be("");
+    }
+
+    [Fact]
+    public void b()
+    {
+        var result = Wrap("this", 10);
+
+        result.Should().Be("this");
+    }
+
+    [Fact]
+    public void c()
+    {
+        var result = Wrap("word", 2);
+
+        result.Should().Be("wo\nrd");
+    }
+
+    [Fact]
+    public void d()
+    {
+        var result = Wrap("abcdefghij", 3);
+
+        result.Should().Be("abc\ndef\nghi\nj");
+    }
+
+    [Fact]
+    public void e()
+    {
+        var result = Wrap("word word", 3);
+
+        result.Should().Be("wor\nd\nwor\nd");
+    }
+
+    [Fact]
+    public void f()
+    {
+        var result = Wrap("word word", 6);
+
+        result.Should().Be("word\nword");
+    }
+
+    [Fact]
+    public void f2()
+    {
+        var result = Wrap("word word", 5);
+
+        result.Should().Be("word\nword");
+    }
+
+    [Fact]
+    public void g()
+    {
+        var result = Wrap("word word word", 6);
+
+        result.Should().Be("word\nword\nword");
+    }
+
+    [Fact]
+    public void h()
+    {
+        var result = Wrap("word word word", 11);
+
+        result.Should().Be("word word\nword");
+    }
+
+    private static string Wrap(string text, int col)
+    {
+        return "";
+    }
+}
